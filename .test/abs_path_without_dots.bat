24d2f9a770c3aac8be5a933b35d7441e9fec9406
Ilya Tretyakov #it3xl <it3xl@users.noreply.github.com>
2018-02-12 15:14:24 +0300

    Loop workaround for removing dots from a path.

--- .test/abs_path_without_dots.bat
+++ .test/abs_path_without_dots.bat
@@ -27,4 +27,13 @@ CALL abs_path_without_dots.bat  ..\dummy.txt
 @ECHO.
 
 
+@ECHO.
+@ECHO.
+@ECHO And don't forget the Loop workaround
+@ECHO.
+@REM Credits: https://stackoverflow.com/questions/1645843/resolve-absolute-path-from-relative-path-and-or-file-name/10018990#10018990
+FOR /F %%i IN ("%~dp0..\dummy.txt") DO ( echo absolute path: %%~fi )
+@ECHO.
+
+
 pause
