ea50e3cff3155eb89c3bebf724e06ffcd39e5e44
Ilya Tretyakov #it3xl <it3xl@users.noreply.github.com>
2018-02-12 14:49:51 +0300

    Removing dots from absolute paths. Converting relative paths to absolute paths related to the current working directory.

---
+++ .test/abs_path_without_dots.bat
@@ -0,0 +1,30 @@
+
+
+@ECHO.
+
+
+CALL ..\abs_path_without_dots.bat  "%~dp0..\dummy.txt"
+@ECHO.
+@ECHO abs_path_without_dots = %abs_path_without_dots%
+
+@ECHO.
+@ECHO.
+@ECHO.
+@ECHO Relateiv input path behaviour
+@ECHO.
+@ECHO CD at beginning is
+@ECHO %CD%
+@ECHO call "CD .."
+@CD ..
+@ECHO CD now is
+@ECHO %CD%
+@ECHO.
+
+@ECHO using of "..\dummy.txt" as an input
+CALL abs_path_without_dots.bat  ..\dummy.txt
+@ECHO.
+@ECHO abs_path_without_dots = %abs_path_without_dots%
+@ECHO.
+
+
+pause
---
+++ abs_path_without_dots.bat
@@ -0,0 +1,22 @@
+@REM Results:
+@REM abs_path_without_dots
+@REM  - global variable that contains an absolute path without dots (without a relative reference).
+
+@REM Usage:
+
+@REM #1 Removes dots from a fully-qualified (absolute) path with dots (with a relative reference https://stackoverflow.com/a/3670775).
+@REM An example 
+@REM "C:\a\b\..\c.txt" -> "C:\a\c.txt"
+
+@REM #2 Converts a relative path to an absolute path related to CD (current working directory).
+@REM An example for "C:\a\b\" current working directory
+@REM "..\c.txt" -> "C:\a\c.txt"
+
+@REM Credits: https://stackoverflow.com/questions/1645843/resolve-absolute-path-from-relative-path-and-or-file-name/3480484#3480484
+
+@SET abs_path_without_dots=%~dpnx1
+
+
+
+
+
