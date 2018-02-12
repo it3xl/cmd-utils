b0bc5cb7d571cd19b5288f22da4aa1f7ff1d629b
Ilya Tretyakov #it3xl <it3xl@users.noreply.github.com>
2018-02-12 18:47:32 +0300

    Allows to delete all items (files or folders) by setting the cleanup_skip_newest_amount parameter to 0.

--- cleanup_old_files.bat
+++ cleanup_old_files.bat
@@ -14,9 +14,17 @@ SET target_cleanup_folder=%target_cleanup_folder:"=%
 
 REM Makes a cleanup for excessive amount of artifact files (any files, not folders).
 ECHO Cleanup in %target_cleanup_folder%
-ECHO Skip the newest %cleanup_skip_newest_amount% items.
-@REM Do not use %%~fF instead of %%F to get a full path! It is unsafe and depend on current executed folder!
-FOR /F "skip=%cleanup_skip_newest_amount% eol=: delims=" %%F IN ('DIR /B /O:-D /A:-D "%target_cleanup_folder%"') DO (
+ECHO Skip %cleanup_skip_newest_amount% newest items.
+
+@REM Credits: "Why does FOR /F “skip=0” fail?"
+@REM https://stackoverflow.com/questions/6452323/why-does-for-f-skip-0-fail/6452839#6452839
+SET SKIP=skip=%cleanup_skip_newest_amount%
+IF %cleanup_skip_newest_amount% LEQ 0 (
+  SET "SKIP="
+)
+
+@REM Do not use %%~fF instead of %%F to get a full path! It is unsafe and depend on the currently executed folder!
+FOR /F "%SKIP% eol=: delims=" %%F IN ('DIR /B /O:-D /A:-D "%target_cleanup_folder%"') DO (
   @REM ! SET= do not works in loops!
   ECHO deleting %target_cleanup_folder%\%%F
   DEL /Q /F "%target_cleanup_folder%\%%F"
