1e059819b19697d85926c487aab2c389538b7f81
Ilya Tretyakov #it3xl <it3xl@users.noreply.github.com>
2018-03-02 15:51:06 +0300

    Registry. Disables implicit redirections to Wow6432Node branch on x64 systems under x86 processes.

--- reg_delete_key.bat
+++ reg_delete_key.bat
@@ -7,7 +7,11 @@ IF ["%reg_key%"] EQU [""] EXIT 1001
 
 @ECHO deletion for: "%reg_key%"
 
-REG DELETE "%reg_key%" /f
+@REM /reg:64 - Disables implicit redirections to Wow6432Node branch on x64 systems under x86 processes.
+@REM So, you forced here to use explicit keys. As an example:
+@REM For x86 apps on x64 systems you must use "Wow6432Node" like this HKLM\Software\Wow6432Node\...
+
+REG DELETE "%reg_key%" /f  /reg:64
 
 
 @REM Will exit with 0 exit code to prevent spam to ERRORLEVEL if process not started.
