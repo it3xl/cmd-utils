9b82fdbe7abf29cf5e50bbae751204c9a18157da
Ilya Tretyakov #it3xl <it3xl@users.noreply.github.com>
2018-08-24 23:02:02 +0300

    Adding a date-time stump to logging.

--- exit_if_error.bat
+++ exit_if_error.bat
@@ -1,5 +1,5 @@
 @IF %ERRORLEVEL% NEQ 0 (
-  @ECHO Exit with an error. CMD has ERRORLEVEL = %ERRORLEVEL%.
+  @ECHO Exit with an error. CMD has ERRORLEVEL = %ERRORLEVEL%. At %DATE% %TIME%
   @REM We will use the EXIT command to inform any Build-Server with a status of execution (0 means no errors).
   EXIT %ERRORLEVEL%
 )
