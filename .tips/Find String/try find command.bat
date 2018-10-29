c903411f99060dfea54e835a8f5615e69fb51acd
Ilya Tretyakov #it3xl <it3xl@users.noreply.github.com>
2018-10-29 16:55:01 +0300

    Suppresses unused errors.

--- .tips/Find String/try find command.bat
+++ .tips/Find String/try find command.bat
@@ -4,7 +4,9 @@
 
 SET env_not_local_path=1
 ECHO  C:\Program Files\some folder | FIND /C /I "%ProgramW6432%">NUL && SET env_not_local_path=0
+IF %ERRORLEVEL% EQU 1  CALL REM Suppresses previous errors.
 ECHO  C:\Program Files\some folder | FIND /C /I "%ProgramFiles%">NUL && SET env_not_local_path=0
+IF %ERRORLEVEL% EQU 1  CALL REM Suppresses previous errors.
 
 @ECHO:
 @ECHO env_not_local_path = %env_not_local_path%
