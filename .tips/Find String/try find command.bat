be3ee316185f472319f977ae1c57fc9defecb5e3
Ilya Tretyakov #it3xl <it3xl@users.noreply.github.com>
2018-10-29 17:26:08 +0300

    Find String Approaches' enhancements.

--- .tips/Find String/try find command.bat
+++ .tips/Find String/try find command.bat
@@ -1,14 +1,62 @@
 @SET invokePath=%~dp0.
 @CD /D %SystemDrive%\
-@ECHO:
 
-SET env_not_local_path=1
-ECHO  C:\Program Files\some folder | FIND /C /I "%ProgramW6432%">NUL && SET env_not_local_path=0
-IF %ERRORLEVEL% EQU 1  CALL REM Suppresses previous errors.
-ECHO  C:\Program Files\some folder | FIND /C /I "%ProgramFiles%">NUL && SET env_not_local_path=0
-IF %ERRORLEVEL% EQU 1  CALL REM Suppresses previous errors.
 
 @ECHO:
-@ECHO env_not_local_path = %env_not_local_path%
+@ECHO:
+@ECHO @ Not match. A usage with a target error suppressing.
+@ECHO:
+SET env_local_path=0
+ECHO  C:\oppa\some folder | FIND /C /I "%ProgramW6432%">NUL || ( SET env_local_path=1 & CALL REM Suppresses previous errors. )
+ECHO  C:\oppa\some folder | FIND /C /I "%ProgramFiles%">NUL || ( SET env_local_path=1 & CALL REM Suppresses previous errors. )
+
+@ECHO:
+@ECHO env_local_path = %env_local_path%
+@ECHO ERRORLEVEL = %ERRORLEVEL%
+
+
+@ECHO:
+@ECHO:
+@ECHO @ Match. A usage with a target error suppressing.
+@ECHO:
+SET env_local_path=0
+ECHO  C:\Program Files\some folder | FIND /C /I "%ProgramW6432%">NUL || ( SET env_local_path=1 & CALL REM Suppresses previous errors. )
+ECHO  C:\Program Files\some folder | FIND /C /I "%ProgramFiles%">NUL || ( SET env_local_path=1 & CALL REM Suppresses previous errors. )
+
+@ECHO:
+@ECHO env_local_path = %env_local_path%
+@ECHO ERRORLEVEL = %ERRORLEVEL%
+
+
+@ECHO:
+@ECHO:
+@ECHO @ Match. A usage with an unsafe error suppressing.
+@ECHO:
+SET env_local_path=1
+ECHO  C:\Program Files\some folder | FIND /C /I "%ProgramW6432%">NUL && SET env_local_path=0
+IF %ERRORLEVEL% EQU 1  CALL REM Suppresses previous errors.
+ECHO  C:\Program Files\some folder | FIND /C /I "%ProgramFiles%">NUL && SET env_local_path=0
+IF %ERRORLEVEL% EQU 1  CALL REM Suppresses previous errors.
+
+@ECHO:
+@ECHO env_local_path = %env_local_path%
+@ECHO ERRORLEVEL = %ERRORLEVEL%
+
+
+@ECHO:
+@ECHO:
+@ECHO @ Not match. A usage with an unsafe error suppressing.
+@ECHO:
+SET env_local_path=1
+ECHO  C:\oppa\some folder | FIND /C /I "%ProgramW6432%">NUL && SET env_local_path=0
+IF %ERRORLEVEL% EQU 1  CALL REM Suppresses previous errors.
+ECHO  C:\oppa\some folder | FIND /C /I "%ProgramFiles%">NUL && SET env_local_path=0
+IF %ERRORLEVEL% EQU 1  CALL REM Suppresses previous errors.
+
+@ECHO:
+@ECHO env_local_path = %env_local_path%
+@ECHO ERRORLEVEL = %ERRORLEVEL%
+
+
 
 PAUSE
