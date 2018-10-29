07c37ed18623b266bc54b122c4587a4fa6cb935c
Ilya Tretyakov #it3xl <it3xl@users.noreply.github.com>
2018-10-29 17:42:16 +0300

    Bug in Find String Approaches' enhancements. I forget that we will not see %ERRORLEVEL% until we won't exit an executing block.

--- .tips/Find String/try find command.bat
+++ .tips/Find String/try find command.bat
@@ -4,33 +4,7 @@
 
 @ECHO:
 @ECHO:
-@ECHO @ Not match. A usage with a target error suppressing.
-@ECHO:
-SET env_local_path=0
-ECHO  C:\oppa\some folder | FIND /C /I "%ProgramW6432%">NUL || ( SET env_local_path=1 & CALL REM Suppresses previous errors. )
-ECHO  C:\oppa\some folder | FIND /C /I "%ProgramFiles%">NUL || ( SET env_local_path=1 & CALL REM Suppresses previous errors. )
-
-@ECHO:
-@ECHO env_local_path = %env_local_path%
-@ECHO ERRORLEVEL = %ERRORLEVEL%
-
-
-@ECHO:
-@ECHO:
-@ECHO @ Match. A usage with a target error suppressing.
-@ECHO:
-SET env_local_path=0
-ECHO  C:\Program Files\some folder | FIND /C /I "%ProgramW6432%">NUL || ( SET env_local_path=1 & CALL REM Suppresses previous errors. )
-ECHO  C:\Program Files\some folder | FIND /C /I "%ProgramFiles%">NUL || ( SET env_local_path=1 & CALL REM Suppresses previous errors. )
-
-@ECHO:
-@ECHO env_local_path = %env_local_path%
-@ECHO ERRORLEVEL = %ERRORLEVEL%
-
-
-@ECHO:
-@ECHO:
-@ECHO @ Match. A usage with an unsafe error suppressing.
+@ECHO @ Match.
 @ECHO:
 SET env_local_path=1
 ECHO  C:\Program Files\some folder | FIND /C /I "%ProgramW6432%">NUL && SET env_local_path=0
@@ -45,13 +19,13 @@ IF %ERRORLEVEL% EQU 1  CALL REM Suppresses previous errors.
 
 @ECHO:
 @ECHO:
-@ECHO @ Not match. A usage with an unsafe error suppressing.
+@ECHO @ Not match.
 @ECHO:
 SET env_local_path=1
 ECHO  C:\oppa\some folder | FIND /C /I "%ProgramW6432%">NUL && SET env_local_path=0
-IF %ERRORLEVEL% EQU 1  CALL REM Suppresses previous errors.
+IF %ERRORLEVEL% EQU 1  CALL ECHO Suppresses previous errors.
 ECHO  C:\oppa\some folder | FIND /C /I "%ProgramFiles%">NUL && SET env_local_path=0
-IF %ERRORLEVEL% EQU 1  CALL REM Suppresses previous errors.
+IF %ERRORLEVEL% EQU 1  CALL ECHO Suppresses previous errors.
 
 @ECHO:
 @ECHO env_local_path = %env_local_path%
