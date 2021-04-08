af439792960a834f935ad3cc434047e74ff7e27a
Ilya Tretyakov #it3xl <it3xl@users.noreply.github.com>
2021-04-08 08:59:22 +0300

    Improving of comments for my delaying approach.

--- delay-seconds.bat
+++ delay-seconds.bat
@@ -11,13 +11,18 @@ IF [%seconds%] EQU [] (
 @ECHO Delay for %seconds% seconds.
 
 @REM Non-Interactive process/account troubles.
+@REM
 @REM @CHOICE /N /C y /D y /T %seconds% > NUL
-@REM It fails with ERROR: The file is either empty or does not contain the valid choices.
+@REM CHOICE fails with ERROR: The file is either empty or does not contain the valid choices.
+@REM Also, it hangs in VSCode.
+@REM
 @REM TIMEOUT  /t %seconds% > NUL
-@REM It fails with ERROR: Input redirection is not supported, exiting the process immediately.
+@REM TIMEOUT fails with an ERROR: Input redirection is not supported, exiting the process immediately.
 @REM Workaround: https://stackoverflow.com/questions/1672338/how-to-sleep-for-five-seconds-in-a-batch-file-cmd/33286113#33286113
-@REM W32TM /stripchart /computer:localhost /period:%seconds% /Dataonly /samples:2 >nul
-@REM 
+@REM
+@REM W32TM /stripchart /computer:localhost /period:%seconds% /Dataonly /samples:2 > NULL
+@REM W32TM is interesting approach but just do not use it for now.
+@REM
 TypePerf "\System\Processor Queue Length" -si %seconds% -sc 1 >nul
 
 
