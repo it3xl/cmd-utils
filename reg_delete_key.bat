1f17d5bbf339ef7cb628c6594bd959bfad2773f1
Ilya Tretyakov #it3xl <it3xl@users.noreply.github.com>
2021-04-15 13:59:55 +0300

    More explicit code for REG DELETE operations.

--- reg_delete_key.bat
+++ reg_delete_key.bat
@@ -7,16 +7,16 @@ IF ["%reg_key%"] EQU [""] EXIT 1001
 
 @ECHO deletion for: "%reg_key%"
 
-@REM /reg:64 - Disables implicit redirections to Wow6432Node branch on x64 systems under x86 processes.
+@REM /reg:64 - Disables implicit redirection to Wow6432Node branch on x64 systems under a x86 processes.
 @REM So, you forced here to use explicit keys. As an example:
 @REM For x86 apps on x64 systems you must use "Wow6432Node" like this HKLM\Software\Wow6432Node\...
 
 REG DELETE "%reg_key%" /f  /reg:64
 
+@REM Let's add an indentation if an error is emitted.
 IF %ERRORLEVEL% NEQ 0  ECHO:
 
 
-@REM Will exit with 0 exit code to prevent spam to ERRORLEVEL if process not started.
-@EXIT /B 0
-
+@REM It is decided that this always suppress REG DELETE errors.
+@CALL REM Suppress previous CMD errors.
 
